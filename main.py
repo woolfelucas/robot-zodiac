079fde9b20af5c4f3863821ae58ab753ddbcfce9
gbtsui <gabriel.tsui@protonmail.com>
2024-12-19 17:14:55 -0500

    skibidi toilet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-import csv
 from classify import classify
+import numpy
+
+numpy.set_printoptions(suppress = True)
+
+import pandas
 
 # actual code starts here
 
@@ -9,7 +12,26 @@ data_file_path = "./data.csv"
 
 output_file = open(output_file_path, "w")
 
+datafile = pandas.read_csv(data_file_path)
+arrays = pandas.DataFrame.to_numpy(datafile)
+
+amp_max = max(filter(lambda x: not numpy.isnan(x), arrays[:, 1]))
+amp_misses_max = max(filter(lambda x: not numpy.isnan(x), arrays[:, 2]))
+speaker_max = max(filter(lambda x: not numpy.isnan(x), arrays[:, 3]))
+speaker_misses_max = max(filter(lambda x: not numpy.isnan(x), arrays[:, 4]))
+shuttle_max = max(filter(lambda x: not numpy.isnan(x), arrays[:, 5]))
+
+amp = arrays[:, 1]/amp_max
+amp_misses = arrays[:, 2]/amp_misses_max
+speaker = arrays[:, 3]/speaker_max
+speaker_misses = arrays[:, 4]/speaker_misses_max
+shuttle = arrays[:, 5]/shuttle_max
+
+print(amp, "\n", amp_misses, "\n", speaker, "\n", speaker_misses, "\n", shuttle)
+
+
 def classify_individual():
+    '''
     with open(data_file_path, newline='') as csvfile:
         dataCSV = csv.reader(csvfile)
         for lines in dataCSV:
@@ -17,6 +39,11 @@ def classify_individual():
                 int_lines = [int(entry) for entry in lines]
                 output_file.write(str(classify(int_lines))+"\n")
     print("classify_individual() complete, log:" + output_file_path)
+    '''
+
+    for i in arrays.shape[1]:
+
+
 
 def get_team_list():
     with open(data_file_path, newline='') as csvfile:
@@ -28,13 +55,14 @@ def get_team_list():
         print(teams)
 
 def classify_average():
-    with open(data_file_path, newLine='') as csvfile:
+    with open(data_file_path, newline='') as csvfile:
         dataCSV = csv.reader(csvfile)
         for lines in dataCSV:
             if lines[0].find("#") == -1:
                 break # FIX LATER AAAAUGH
 
-get_team_list()
+
+#get_team_list()
 classify_individual()
 
 '''
